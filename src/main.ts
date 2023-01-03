58441fad2308de6215ed4353b430b4939c720b27
zhiwen.li <674356245@qq.com>
2023-01-03 16:18:36 +0800

    事件监听

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import Attribute from './Attribute.vue'
+import On from './On.vue'
 
 import './assets/main.css'
 
-createApp(Attribute).mount('#app')
+createApp(On).mount('#app')
