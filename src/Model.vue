0958bb22df4d27f1882c0e1d7d65da79b13bdb1a
zhiwen.li <674356245@qq.com>
2023-01-03 16:27:30 +0800

    表单绑定

---
+++ src/Model.vue
@@ -0,0 +1,14 @@
+
+<script setup lang="ts">
+import {ref} from 'vue';
+const text = ref('');
+function onInput(e:any){
+    text.value = e.target.value;
+}
+
+</script>
+
+<template>
+    <input v-model="text" placeholder="Type here">
+    <p>{{ text }}</p>
+</template>
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import Attribute from './Attribute.vue'
 import On from './On.vue'
+import Model from './Model.vue'
 
 import './assets/main.css'
 
-createApp(On).mount('#app')
+//createApp(On).mount('#app')
+createApp(Model).mount('#app')
